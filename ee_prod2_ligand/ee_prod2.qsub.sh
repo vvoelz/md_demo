6edd5c0ebe5a39c7c0161906f7bd40acdd3f422c
Vincent Voelz <vvoelz@gmail.com>
2024-04-21 21:31:20 -0400

    added -maxh 35.95

--- ee_prod2_ligand/ee_prod2.qsub.sh
+++ ee_prod2_ligand/ee_prod2.qsub.sh
@@ -8,11 +8,10 @@
 #PBS
 cd $PBS_O_WORKDIR
 
-bash ~/.bashrc
 module load gromacs/2020.3
 
 # note --using npt.gro as starting structure since it's compatible with lam_k=0 ensemble
 gmx grompp -f ee_prod2.mdp -c npt.gro -r em_solv_ions.gro -p ligand.top -o ee_prod2.tpr -v -maxwarn 2
-gmx mdrun -v -s ee_prod2.tpr -c ee_prod2.gro -g ee_prod2.log -o ee_prod2.trr -x ee_prod2.xtc -dhdl ee_prod2.dhdl
+gmx mdrun -v -maxh 35.95 -s ee_prod2.tpr -c ee_prod2.gro -g ee_prod2.log -o ee_prod2.trr -x ee_prod2.xtc -dhdl ee_prod2.dhdl
 
  
